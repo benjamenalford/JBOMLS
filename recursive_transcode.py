30e24f132a5ac209b3f50a82d0e8f5eb9f6932b5
Benjamen Alford <benajamen.alford@gmail.com>
2022-11-29 14:08:48 -0600

    tabs

--- recursive_transcode.py
+++ recursive_transcode.py
@@ -8,11 +8,11 @@ destination_codec = 'alac'
 
 def main():
 	args = term_args()
-	
+
 	if args.path is None or args.output_path is None:
-        	print('input path or output path not specified')
-        	exit(1)
-		
+			print('input path or output path not specified')
+			exit(1)
+
 	base_dir = args.path
 	output_dir =  args.output_path
 	print(f'Base Directory  = { base_dir}')
@@ -22,7 +22,7 @@ def main():
 
 	for root, subs, files in os.walk(base_dir):
 		for file in files:
-			if file.endswith(lossless_extensions):	
+			if file.endswith(lossless_extensions):
 				convert(file, root, output_dir)
 
 def convert(file, path, output_dir):
@@ -34,19 +34,19 @@ def convert(file, path, output_dir):
 
 	for file_extension in lossless_extensions:
 		if (file.endswith(file_extension)):
-			out_file = file.split(file_extension)[0] 
+			out_file = file.split(file_extension)[0]
 	cmd = f'ffmpeg -i "{os.path.join(path , file)}" -y -v 0 -vcodec copy -acodec {destination_codec} "{os.path.join(destination_folder,out_file)}.m4a"'
-	
-	os.system(cmd)
-	
-def term_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", type=str, help="Path to be transcoded.")
-    parser.add_argument("-o", "--output_path", type=str, help="Output path")
-    parser.add_argument("-c", "--output_codec", type=str, help="Output codec")
-    parser.add_argument("-i", "--inplace", type=str, help="Transcode In Place")
-    parser.add_argument("-d", "--debug", type=str, help="debug mode, will not change anything")
-    return parser.parse_args()
 
-if __name__ == '__main__': 
-    main()
+	os.system(cmd)
+
+def term_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--path", type=str, help="Path to be transcoded.")
+	parser.add_argument("-o", "--output_path", type=str, help="Output path")
+	parser.add_argument("-c", "--output_codec", type=str, help="Output codec")
+	parser.add_argument("-i", "--inplace", type=str, help="Transcode In Place")
+	parser.add_argument("-d", "--debug", type=str, help="debug mode, will not change anything")
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	main()
