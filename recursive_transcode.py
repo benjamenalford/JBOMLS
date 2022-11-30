86027305fe6792024f77d164ea90984c82b0bcb2
Benjamen Alford <benajamen.alford@gmail.com>
2022-11-30 10:53:14 -0600

    change inplace

--- recursive_transcode.py
+++ recursive_transcode.py
@@ -15,7 +15,7 @@ def main():
 		print("Debug mode on")
 		print(args)
 
-	if args.path is None and (args.output_path is None and args.inplace is True):
+	if args.path is None or (args.output_path is None and args.inplace is False):
 			print('input path or output path not specified')
 			exit(1)
 
