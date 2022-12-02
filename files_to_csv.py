c1fdb395074136cfbee02eb8198e445ed322ddfb
Benjamen Alford <benajamen.alford@gmail.com>
2022-12-02 08:37:27 -0600

    building file list

--- files_to_csv.py
+++ files_to_csv.py
@@ -1,14 +1,18 @@
 import argparse
 import os
+import csv
+import mutagen
 
 debug = False
 output_csv = ""
 base_dir = ""
+file_extensions = (".flac", ".wav",".mp3", ".m4a", ".aiff")
 
 def main():
 	global debug
 	global output_csv
 	global base_dir
+	global file_extensions
 
 	args = term_args()
 
@@ -16,6 +20,15 @@ def main():
 			print('input path not specified, using current folder as base')
 			exit(1)
 
+	for root, subs, files in os.walk(base_dir):
+		for file in files:
+			if file.endswith(file_extensions):
+				file_info = get_file_info(file)
+
+
+def get_file_info():
+	info = mutagen.File("11. The Way It Is.ogg")
+	return info
 
 def term_args():
 	parser = argparse.ArgumentParser(prog='files_to_csv', description='Builds CSV of files')
